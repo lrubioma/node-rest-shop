56a0e83b9f4400ba9077135d40305bcbe6b4cff3
Luis Fernando Rubio Martínez <Lrubioma@everis.com>
2018-11-11 14:58:18 +0100

    change 1 in app.js. branch v2_beta

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-//Commnet this
+//Commnet included in debugging branch
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin","*");
   res.header(
